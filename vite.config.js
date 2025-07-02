6309df24946ea48b73dc4b48bda4d99bee55372f
Scholzova <99414133+Scholzova@users.noreply.github.com>
2025-07-02 10:57:06 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Timora/", // musí být přesně podle názvu repo
+  base: "/timora/", // musí být přesně podle názvu repo
 });
