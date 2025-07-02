f6aa4495982bc7df5ba5cd8bb1639a59656b637a
Scholzova <99414133+Scholzova@users.noreply.github.com>
2025-07-02 10:53:17 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { HashRouter } from "react-router-dom"; // <-- důležité pro GitHub Pages
 
 const theme = createTheme({
   palette: {
@@ -30,9 +31,11 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <HashRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </HashRouter>
   </React.StrictMode>
 );
